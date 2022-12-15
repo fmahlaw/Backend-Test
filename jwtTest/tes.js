704b718fd879bde7cc075c036158b3a1aada0336
fmahlaw <ferianmahlaw@gmail.com>
2022-12-16 00:19:56 +0700

    v1 unprettier

---
+++ jwtTest/tes.js
@@ -0,0 +1,14 @@
+const a = [{
+
+    description: "Ferian",
+    id :"ABC"
+
+}]
+
+const c = '32bf67e5-4971-47ce-985c-44b6b3860cdb'
+
+let b = a[0].description.toLowerCase()
+
+
+
+console.log(c.toUpperCase())
